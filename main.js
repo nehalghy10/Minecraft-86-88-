0f4829f0fb1c9b426bd92f29e914c085830afaf5
nehalghy10 <73056309+nehalghy10@users.noreply.github.com>
2021-10-21 19:39:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -46,9 +46,17 @@ function my_keydown(e)
     console.log(keyPressed);
     if(e.shiftKey == true && keyPressed == '80')
     {
-        console.log("p and shifted pressed together");
+        console.log("p and shift pressed together");
         block_image_width = block_image_width + 10;
         block_image_height = block_image_height + 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if(e.shiftKey == true && keyPressed == '77')
+    {
+        console.log("m and shift pressed together");
+        block_image_width = block_image_width - 10;
+        block_image_height = block_image_height - 10;
         document.getElementById("current_width").innerHTML = block_image_width;
         document.getElementById("current_height").innerHTML = block_image_height;
     }
@@ -142,7 +150,7 @@ function down ()
 }
 function left ()
 {
-     if(player_x >=0)
+     if(player_x >0)
      {
           player_x = player_x - block_image_width;
           console.log("block image width = " + block_image_width);
@@ -155,7 +163,7 @@ function right ()
 {
      if(player_x <=850)
      {
-          player_x = player_x - block_image_width;
+          player_x = player_x + block_image_width;
           console.log("block image height = " + block_image_width);
           console.log ("When Right Arrow Is preesed, X = " + player_x +" , Y = " +player_y);
           canvas.remove(player_object);
